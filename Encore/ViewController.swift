d7c0d023ffca83e93d9fa9f544a7154415635e3d
Jenny Guzdial <jguzdial6@gatech.edu>
2019-07-29 22:02:04 -0500

    Initial Commit

---
+++ Encore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Encore
+//
+//  Created by Jenny Guzdial on 7/29/19.
+//  Copyright © 2019 Jenny Guzdial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
