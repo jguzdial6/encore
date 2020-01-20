9f106fcb674c2d908044cd800365a47224159e94
Jenny Guzdial <jguzdial6@gatech.edu>
2020-01-20 18:36:31 -0500

    Added arrays for new words to appear with button press

--- Encore/ViewController.swift
+++ Encore/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    let words: [String] = ["Rain", "Men", "Glory", "Sky", "Wet", "Tonight", "Together", "Apart", "Dog", "Car"]
+    var remainingWords = [String]()
+    @IBOutlet weak var WordBox: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //MARK: Actions
 
-
+    @IBAction func WordButtonPress(_ sender: UIButton) {
+        self.WordBox.text = self.GetNextWord()
+        
+    }
+    func GetNextWord() -> String {
+        debugPrint(remainingWords)
+        if remainingWords.count == 0{
+            remainingWords = words
+        }
+        let nextWord: String = remainingWords.randomElement() ?? "f"
+        remainingWords.remove(at: remainingWords.firstIndex(of: nextWord)!)
+        return nextWord
+    }
 }
 
